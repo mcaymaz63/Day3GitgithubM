f6192b34e667d1ea64f6a9331aa7e49b24e78bee
mevlut caymaz <mevlitkacmaz.63@gmail.com>
2022-08-16 10:06:42 +0300

    M1

---
+++ src/Day3GitGithub.java
@@ -0,0 +1,71 @@
+public class Day3GitGithub {
+  /*
+
+
+
+
+
+   /*
+1-) git init --> Local repo oluşturmak için yani .git ile klasörümüzün
+içindeki dosyaları ilişkilendirmek için kullanılır
+2-) git add . --> Working space'den (yani yerel) dosyalarımı staging area'ya
+(yani commitlemek için beklenen yer) gönderir
+3-) git commit -m "mesaj" --> Staging area'dan commit stora
+dosyalarımı göndermek için kullanılır (commit yani versiyon
+yani sürüm oluşturmuş olurum)
+4-) git push --> Uzak repo'ya(yani remote - Github)
+göndermek için kullandığımız kod Yalnız git push komutunu direk kullanmak istersek 1 kez
+            git remote add origin adres
+            git push -u origin master
+Not: Yukarıdaki iki komutu tek seferde kullandıktan sonra ikinci commit'lerim için sadece git push
+kullanırız
+   İlk defa PUSH yapacaksanız bir projeyi ;
+
+1 - Önce GitHub a gidip New Repository i oluşturun
+2- Oluşturduğunuz repository de size verilen https://github.com/.............git   adresini kopyalayın
+
+
+    Şimdi IntelliJ ye gelip TERMINAL ı açın;
+
+3- git init yazın
+4- git add . yazın
+5- git commit -m"first commit"  (first commit yerine  ilke versiyon, ilk ders veya istediğiniz bir şeyi yazın)
+            6- git branch -M main
+7- git remote add origin https://github.com/.............git (yukarıda2. satırda kopyalağınız adresi buraya yapıştırın)
+            8- git push -u origin main
+
+
+    SONRASINDA Aynı PROJE için ;
+
+    her yeni eklediğiniz şeyden sonra GitHub'a yüklemek için
+
+            9- git add .
+            10 - git commit -m"YENI NUMARA VEYA YAZI YAZ"
+            11 - git push
+
+
+    hepsi bu kadar
+
+Dosyayı İlk defa göndereceğinizde
+1) git init
+2) git add .
+3) git commit -m "versiyon_adi"
+4) git remote add origin https://github.com/.....
+5) git push -u origin master
+
+
+Kodlarınızda değişiklik yaptığınızda tekrar güncellemek için
+1) git add .
+2) git commit -m "versiyon_adı"
+3) git push
+
+
+        */
+
+
+
+
+
+
+
+}
